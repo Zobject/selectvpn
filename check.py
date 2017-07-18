6e1b0a6e34d96a96bbb2213606f1a7229e891a43
Zobject <18920354189@163.com>
2017-07-18 14:24:50 +0800

    Summary

--- check.py
+++ check.py
@@ -1,6 +1,10 @@
 import pymongo
 from  datetime import datetime
 import time
+import httplib
+from  itsdangerous import URLSafeTimedSerializer
+
+
 
 try:
     conn=pymongo.MongoClient()
@@ -9,6 +13,13 @@ except:
     print 'fail'
 
 db=conn['VPN']
+
+
+
+def encrypt1(port):
+    rule=URLSafeTimedSerializer('secret-key',salt='hello')
+    token=rule.dumps({'port':port})
+    return token
 
 
 def check():
@@ -26,10 +37,22 @@ def check():
             print Host
             #14400
             if (now-checktime).seconds>14400:
-                # print (now-checktime).seconds
+                print (now - checktime).seconds
                 # print now
                 print "+++++++"
-                collection.update({'port': port, 'ip': Host},
+                send_data = encrypt1(port)
+                url = 'http://' + Host + ':1200/token=' + send_data
+                print url
+                # 连接服务器
+                conn = httplib.HTTPConnection(Host + ':1200')
+                # 发送get请求
+                conn.request(method="GET", url=url)
+                # 获得返回数据
+                response = conn.getresponse()
+                res = response.read()
+                # 根据节点服务的返回进行不同的操作，如果返回成功对数据库进行更新将端口更新为可用，并且通知客户端。
+                if res == 'success':
+                    collection.update({'port': port, 'ip': Host},
                                   {'$set': {'target': 0, 'password': '3213213', 'time': datetime.now()}})
         # time.sleep(1800)
 
@@ -48,7 +71,19 @@ def check():
                 print (now - checktime).seconds
                 # print now
                 print "+++++++"
-                collection.update({'port': port, 'ip': Host},
+                send_data = encrypt1(port)
+                url = 'http://' + Host + ':1200/token=' + send_data
+                print url
+                # 连接服务器
+                conn = httplib.HTTPConnection(Host + ':1200')
+                # 发送get请求
+                conn.request(method="GET", url=url)
+                # 获得返回数据
+                response = conn.getresponse()
+                res = response.read()
+                # 根据节点服务的返回进行不同的操作，如果返回成功对数据库进行更新将端口更新为可用，并且通知客户端。
+                if res == 'success':
+                    collection.update({'port': port, 'ip': Host},
                                   {'$set': {'target': 0, 'password': '3213213', 'time': datetime.now()}})
             else:
                 print (now - checktime).seconds
@@ -70,7 +105,19 @@ def check():
                 print (now - checktime).seconds
                 # print now
                 print "+++++++"
-                collection.update({'port': port, 'ip': Host},
+                send_data = encrypt1(port)
+                url = 'http://' + Host + ':1200/token=' + send_data
+                print url
+                # 连接服务器
+                conn = httplib.HTTPConnection(Host + ':1200')
+                # 发送get请求
+                conn.request(method="GET", url=url)
+                # 获得返回数据
+                response = conn.getresponse()
+                res = response.read()
+                # 根据节点服务的返回进行不同的操作，如果返回成功对数据库进行更新将端口更新为可用，并且通知客户端。
+                if res == 'success':
+                    collection.update({'port': port, 'ip': Host},
                                   {'$set': {'target': 0, 'password': '3213213', 'time': datetime.now()}})
             else:
                 print (now - checktime).seconds
